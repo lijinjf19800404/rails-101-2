73da1943e501f44fa0ce63374474ad15991e3dd6
lijinjf <lijinjf@qq.com>
2016-12-31 12:11:43 +0800

    add user_id to group

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable， :validatable
+         :recoverable, :rememberable, :trackable, :validatable
 
   has_many :groups
 
